a350b9cb4e11e616612754003b18acca5c92980e
ulrmdhn <auliarama965@gmail.com>
2023-04-06 14:50:38 +0700

    update

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -97,7 +97,7 @@ function Navbar() {
           <button
             onClick={() => setIsOpen(!isOpen)}
             type="button"
-            className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white absolute top-[31rem] left-[5rem]"
+            className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white absolute top-[31] left-[5rem]"
             aria-controls="mobile-menu"
             aria-expanded="false"
           >
