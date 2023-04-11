11da01781eafbcb56aa13188424e6a00ccdd5748
ulrmdhn <auliarama965@gmail.com>
2023-04-11 14:39:35 +0700

    update

--- src/pages/details/[id].js
+++ src/pages/details/[id].js
@@ -12,29 +12,61 @@ function details() {
       .get(`https://digimon-api.com/api/v1/digimon/${id}`)
       .then((res) => {
         setDetail(res.data);
-        
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  console.log('details ==>', details)
+  console.log("details ==>", details);
+
+  if (details.length === 0) {
+    return null;
+  }
+
+  // const buah = [1, 2, 3]
+
+  // console.log('first', buah[2])
+
+  // {details?.descriptions?.map((d) => (
+
+  // ))}
+
+  // console.log('d.description', details?.descriptions)
+
+  // if (details.descriptions === undefined || Array.length == 0) {
+
+  // }
 
   return (
-    <div className="w-screen h-screen bg-blue-300">
+    <div className="w-screen h-screen bg-yellow-400 p-3">
       <div>
-        <div key={details.id} className="detail">
-          <h1>{details.name}</h1>
-          {details?.images?.map((i) => (
-            <img key={details.id} src={i.href} />
-          ))}
-
-          {details?.skills
-            ?.filter((s) => s.skills == 1)
-            .map((s) => (
-              <p>{s.id}</p>
+        <div key={details.id} className="detail bg-white w-3/4 h-screen overflow-scroll m-auto flex flex-col items-center">
+          <div className="">
+            {details?.images?.map((i) => (
+              <img key={details.id} src={i.href} />
             ))}
+          </div>
+
+          <div className="">
+            <h1 className="text-center">{details.name}</h1>
+
+            <div  className="text-center mt-5">
+              <p className="font-bold">Levels</p>
+            {details?.levels?.map((l) => (
+              <p key={l.id}>{l.level}</p>
+            ))}
+            </div>
+
+            <div className="text-center mt-5">
+              <p className="font-bold">Type</p>
+            {details?.types?.map((t) => (
+              <p className="text-center" key={t.id}>{t.type}</p>
+            ))}
+            </div>
+
+            <p>{details.descriptions[0].description}</p>
+          </div>
         </div>
       </div>
     </div>
